045d597385351fd78939111f4cc0ecc4324545ec
Carsten Nilsson <carsten.nilsson@accenture.com>
2017-09-01 14:37:43 +0200

    Readability

--- TriangleDeterminator/Program.cs
+++ TriangleDeterminator/Program.cs
@@ -48,30 +48,69 @@ namespace TriangleDeterminator
                 Console.Write("Insert a number: ");
                 var entry = Console.ReadLine();
 
-                if (entry.ToLower().Trim() == "exit")
-                {
-                    Console.WriteLine("PROGRAM TERMINATED. Press any key to exit.");
-                    Console.ReadLine();
-                    break;
-                }
+                // Validate input
+                if (CheckEmpty(entry)) continue;
+                if (CheckExit(entry)) break;
+                if (CheckNaN(entry, out var result)) continue;
+                if (CheckZero(result)) continue;
+                if (CheckNegative(result)) continue;
 
-                if (double.TryParse(entry, out var result))
-                {
-                    if (result < 1)
-                    {
-                        Console.WriteLine("You cannot use a negative number or zero! Please retry.");
-                        continue;
-                    }
-
-                    entries.Add(result);
-                    Console.WriteLine($"Adding {entry} as triangle side. Current state: [{string.Join(", ", entries)}]");
-                }
-                else
-                {
-                    Console.WriteLine("Input was Not-a-Number! Please retry or type exit.");
-                }
+                // Use input
+                entries.Add(result);
+                Console.WriteLine($"Adding {entry} as triangle side. Current state: [{string.Join(", ", entries)}]");
             }
             return entries;
+        }
+
+        private static bool CheckNaN(string entry, out double result)
+        {
+            if (!double.TryParse(entry, out result))
+            {
+                Console.WriteLine("Input was not valid number! Please retry or type exit.");
+                return true;
+            }
+            return false;
+        }
+
+        private static bool CheckNegative(double result)
+        {
+            if (result < 0)
+            {
+                Console.WriteLine("You cannot have a negative number as input! Try again.");
+                return true;
+            }
+            return false;
+        }
+
+        private static bool CheckZero(double result)
+        {
+            if (result == 0)
+            {
+                Console.WriteLine("You cannot have zero as input! Try again.");
+                return true;
+            }
+            return false;
+        }
+
+        private static bool CheckExit(string entry)
+        {
+            if (entry.ToLower().Trim() == "exit")
+            {
+                Console.WriteLine("PROGRAM TERMINATED. Press any key to exit.");
+                Console.ReadLine();
+                return true;
+            }
+            return false;
+        }
+
+        private static bool CheckEmpty(string entry)
+        {
+            if (string.IsNullOrWhiteSpace(entry))
+            {
+                Console.WriteLine("Please type a number! Try again.");
+                return true;
+            }
+            return false;
         }
     }
 }
